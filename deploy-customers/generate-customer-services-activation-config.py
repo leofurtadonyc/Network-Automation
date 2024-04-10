544a46bc757eecb65baf128d33d0849ace770ff0
leofurtadonyc <166228283+leofurtadonyc@users.noreply.github.com>
2024-04-10 13:40:45 +0100

    Fixed IPv4 and IPv6 address validation, and added support for customer static routes

--- deploy-customers/generate-customer-services-activation-config.py
+++ deploy-customers/generate-customer-services-activation-config.py
@@ -43,20 +43,30 @@ def collect_inputs():
     vlan_id_outer = input("Outer VLAN ID (1-4095): ")
     pw_id = input("Pseudowire ID: ")
     irb_ipaddr = input("IP Address for the BVI or IRB PE interface: ")
-    irb_mask = input("Subnet mask in bits for the BVI or IRB PE interface: ")
     irb_ipv6addr = input("IPv6 address for the BVI or IRB PE interface: ")
-    irb_ipv6mask = input("IPv6 subnet mask in bits for the BVI or IRB PE interface: ")
+    ipv4_lan = input("Customers IPv4 LAN network for the static routes on PE device. Format prefix/mask: ")
+    ipv4_nexthop = input("IPv4 next-hop address to customer's LAN: ")
+    ipv6_lan = input("Customers IPv6 LAN network for the static routes on PE device. Format prefix/mask: ")
+    ipv6_nexthop = input("IPv6 next-hop address to customer's LAN: ")
     pe_device = input("Provider Edge Device Hostname: ")
     service_type = input("Service Type (p2p or p2mp): ")
-    return (customer_name, access_device, access_interface, circuit_id, qos_input, qos_output, vlan_id, vlan_id_outer, pw_id, irb_ipaddr, irb_mask, irb_ipv6addr, irb_ipv6mask, pe_device, service_type)
+    return (customer_name, access_device, access_interface, circuit_id, qos_input, qos_output, vlan_id, vlan_id_outer, pw_id, irb_ipaddr, irb_ipv6addr, ipv4_lan, ipv4_nexthop, ipv6_nexthop, ipv6_lan, pe_device, service_type)
 
-def valid_ip(address):
+def valid_ip_network(network):
+    """Validate an IP network prefix."""
+    try:
+        network_obj = ipaddress.ip_network(network, strict=False)
+        return str(network_obj)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Invalid IP network: {network}")
+
+def valid_ip_address(address):
     """Validate an IP address."""
     try:
-        ip = ipaddress.ip_address(address)
-        return address
+        ip_obj = ipaddress.ip_address(address)
+        return str(ip_obj)
     except ValueError:
-        raise argparse.ArgumentTypeError(f"Invalid IPv4/6 address: {address}")
+        raise argparse.ArgumentTypeError(f"Invalid IP address: {address}")
 
 def main():
     parser = argparse.ArgumentParser(description="Generate service provisioning configurations for network devices.")
@@ -69,10 +79,12 @@ def main():
     parser.add_argument("--vlan-id", type=int, help="VLAN ID (1-4095).")
     parser.add_argument("--vlan-id-outer", type=int, help="VLAN ID (1-4095).")
     parser.add_argument("--pw-id", type=int, help="Pseudowire ID.")
-    parser.add_argument("--irb-ipaddr", type=valid_ip, help="IP address for the BVI or IRB PE interface.")
-    parser.add_argument("--irb-mask", type=int, help="Subnet mask in bits for the BVI or IRB PE interface.")
-    parser.add_argument("--irb-ipv6addr", type=valid_ip, help="IPv6 address for the BVI or IRB PE interface.")
-    parser.add_argument("--irb-ipv6mask", type=int, help="IPv6 subnet mask in bits for the BVI or IRB PE interface.")
+    parser.add_argument("--irb-ipaddr", type=valid_ip_network, help="IP address for the BVI or IRB PE interface.")
+    parser.add_argument("--irb-ipv6addr", type=valid_ip_network, help="IPv6 address for the BVI or IRB PE interface.")
+    parser.add_argument("--ipv4-lan", type=valid_ip_network, help="Customer IPv4 LAN network. Format prefix/mask.")
+    parser.add_argument("--ipv4-nexthop", type=valid_ip_address, help="IPv4 next-hop address to customer's LAN")
+    parser.add_argument("--ipv6-lan", type=valid_ip_network, help="Customer IPv6 LAN network. Format prefix/mask.")
+    parser.add_argument("--ipv6-nexthop", type=valid_ip_address, help="IPv6 next-hop address to customer's LAN")
     parser.add_argument("--pe-device", type=str, help="Hostname of the Provider Edge device.")
     parser.add_argument("--service-type", choices=['p2p', 'p2mp'], help="Service type: point-to-point or point-to-multipoint.")
     parser.add_argument("--interactive", action='store_true', help="Run the script in interactive mode to gather input from the operator.")
@@ -80,7 +92,7 @@ def main():
     args = parser.parse_args()
 
     if args.interactive:
-        args.customer_name, args.access_device, args.access_interface, args.circuit_id, args.qos_input, args.qos_output, args.vlan_id, args.vlan_id_outer, args.pw_id, args.irb_ipaddr, args.irb_mask, args.irb_ipv6addr, args.irb_ipv6mask, args.pe_device, args.service_type = collect_inputs()
+        args.customer_name, args.access_device, args.access_interface, args.circuit_id, args.qos_input, args.qos_output, args.vlan_id, args.vlan_id_outer, args.pw_id, args.irb_ipaddr, args.irb_ipv6addr, args.ipv4_lan, args.ipv6_lan, args.pe_device, args.service_type = collect_inputs()
 
     start_time = time.time()
 
@@ -129,9 +141,11 @@ def main():
         'vlan_id_outer': args.vlan_id_outer,
         'pw_id': args.pw_id,
         'irb_ipaddr': args.irb_ipaddr,
-        'irb_mask': args.irb_mask,
         'irb_ipv6addr': args.irb_ipv6addr,
-        'irb_ipv6mask': args.irb_ipv6mask,
+        'ipv4_lan': args.ipv4_lan,
+        'ipv4_nexthop': args.ipv4_nexthop,
+        'ipv6_lan': args.ipv6_lan,
+        'ipv6_nexthop': args.ipv6_nexthop,
         'access_address': access_device_info['ip_address'],
         'pe_address': pe_device_info['ip_address'],
         'device_type': pe_device_info['device_type']
