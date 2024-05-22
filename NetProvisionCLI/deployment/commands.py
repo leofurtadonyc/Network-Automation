8d6b05a91c4e1911146b4f7ec2a2d18cb51bb004
leofurtadonyc <166228283+leofurtadonyc@users.noreply.github.com>
2024-05-22 12:23:43 +0100

    Refreshing NetProvision CLI

--- NetProvisionCLI/deployment/commands.py
+++
@@ -1,7 +0,0 @@
-def prepare_device_commands(device_type: str, configuration: str) -> list:
-    return {
-        'cisco_xe': ['config terminal', configuration, 'end', 'write memory', 'exit'],
-        'cisco_xr': ['config terminal', configuration, 'commit', 'end', 'exit'],
-        'juniper_junos': ['edit', configuration, 'commit', 'commit and-quit'],
-        'huawei_vrp': ['system-view', configuration, 'return', 'save', 'Y', 'quit']
-    }.get(device_type, [])
--- NetProvisionCLI/utils/template_utils.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-
-def render_template(template_name, context):
-    """Render customer configuration from Jinja2 templates."""
-    env = Environment(loader=FileSystemLoader('templates/'))
-    template = env.get_template(template_name)
-    return template.render(context)
