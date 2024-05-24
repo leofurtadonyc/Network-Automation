ae445afb61aa8bb58177470b6cbb6d8a16672f10
leofurtadonyc <166228283+leofurtadonyc@users.noreply.github.com>
2024-05-24 15:58:45 +0100

    Added sample data to populate MongoDB

--- NetProvisionCLI/sample_populate_mongodb.py
+++ NetProvisionCLI/sample_populate_mongodb.py
@@ -8,7 +8,7 @@ db = client.netprovision
 customers_collection = db.customers
 devices_collection = db.devices
 
-# Example data
+# NetProvisionCLI sample data
 customers_collection.insert_one({
     "name": "LEONARDOFURTADO",
     "customer_details": {
@@ -50,11 +50,60 @@ devices_collection.insert_many([
         "forbidden_interfaces": ["^GigabitEthernet[2-4]$", "^Loopback[0-9999]$"],
     },
     {
+        "device_name": "a-huawei-02",
+        "device_type": "huawei_vrp",
+        "device_role": "access",
+        "ip_address": "192.168.255.12",
+        "loopback": "10.255.255.2",
+        "customer_provisioning": True,
+        "forbidden_interfaces": ["^Ethernet1/0/[3-9]$", "^Loopback[0-9999]$"],
+    },
+    {
+        "device_name": "p-cisco-01",
+        "device_type": "cisco_xe",
+        "device_role": "p",
+        "ip_address": "192.168.255.13",
+        "loopback": "10.255.255.3",
+        "customer_provisioning": False,
+    },
+    {
+        "device_name": "p-cisco-02",
+        "device_type": "cisco_xe",
+        "device_role": "p",
+        "ip_address": "192.168.255.14",
+        "loopback": "10.255.255.4",
+        "customer_provisioning": False,
+    },
+    {
         "device_name": "pe-juniper-01",
         "device_type": "juniper_junos",
         "device_role": "pe",
         "ip_address": "192.168.255.15",
         "loopback": "10.255.255.5",
+        "customer_provisioning": True,
+    },
+    {
+        "device_name": "pe-huawei-02",
+        "device_type": "huawei_vrp",
+        "device_role": "pe",
+        "ip_address": "192.168.255.18",
+        "loopback": "10.255.255.8",
+        "customer_provisioning": False,
+    },
+    {
+        "device_name": "pe-cisco-03",
+        "device_type": "cisco_xe",
+        "device_role": "pe",
+        "ip_address": "192.168.255.16",
+        "loopback": "10.255.255.6",
+        "customer_provisioning": True,
+    },
+    {
+        "device_name": "pe-cisco-04",
+        "device_type": "cisco_xr",
+        "device_role": "pe",
+        "ip_address": "192.168.255.17",
+        "loopback": "10.255.255.7",
         "customer_provisioning": True,
     }
 ])
