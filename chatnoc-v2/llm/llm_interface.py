879ef6fca7f96441fc137f0e08414a13b8d22518
Leonardo Furtado <leofurtadonyc@gmail.com>
2025-02-16 17:20:55 +0000

    Experimenting with differente LLMs for accuracy

--- chatnoc-v2/llm/llm_interface.py
+++ chatnoc-v2/llm/llm_interface.py
@@ -18,6 +18,9 @@ def get_llm():
     config = load_llm_config()
     # Set default base_url and model if not provided in config.
     base_url = config.get("base_url", "http://192.168.0.213:11434")
+    # model = config.get("model", "deepseek-coder:6.7b")
+    # model = config.get("model", "deepseek-r1:8b")
+    # model = config.get("model", "llama3.2")
     model = config.get("model", "llama3.1:8b")
     llm = OllamaLLM(model=model, base_url=base_url)
     return llm
