9b25cd4c0d9d98241778505fd5069d9180d4c95b
leofurtadonyc <166228283+leofurtadonyc@users.noreply.github.com>
2025-03-19 00:36:36 +0000

    Removed local_irrd to a new repository (see IRR-RPSL-Dispatcher)

--- local_irrd/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting IRRd in foreground..."
-irrd --config /etc/irrd/irrd.yaml --foreground
-echo "IRRd has exited. Keeping container alive for debugging..."
-# Sleep indefinitely so the container doesn’t exit immediately.
-tail -f /dev/null
